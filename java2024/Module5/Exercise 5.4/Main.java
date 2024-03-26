11e41fba4cf608e011cf5f3c8dd89be745df0147
rsaavedravaldivia <156375634+rsaavedravaldivia@users.noreply.github.com>
2024-03-26 11:27:26 -0400

    Add files via upload

---
+++ java2024/Module5/Exercise 5.4/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    
+
+
+    public static void main(String[] args) {
+
+        double mileToKilomer = 1.609;
+
+        System.out.println("Miles Kilometers");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i +" "+ i * mileToKilomer);
+        }
+        
+    }
+}
